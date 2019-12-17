76f6a707b60933cf241fc85a5b556d0d20242dcb
Islam Muraev <gladskull@yandex.ru>
2019-12-17 22:48:43 +0400

    18

--- app.js
+++ app.js
@@ -1,20 +1,13 @@
 const express = require("express");
   const app = express();
-app.get("/", function(request, response){
-          response.send("<h1>Главная страница</h1>");
-});
-app.use("/about", function(request, response){
-      
-    console.log(request.query);
-    let names = request.query.name;
-    let responseText = "<ul>";
-    for(let i=0; i < names.length; i++){
-        responseText += "<li>" + names[i] + "</li>";
-    }
-    responseText += "</ul>";
-    response.send(responseText);
+ app.use("/about", function(request, response){
+         console.log(request.query);
+    let id = request.query.user.id;
+    let name = request.query.user.name;
+        response.send("<h3>id:" + id + "<br>name: " + name +"</h3>");
 });
  app.listen(3000);
+
 
 
 
