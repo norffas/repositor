62af64819add3000c1a5617906438888b3e1989b
Islam Muraev <gladskull@yandex.ru>
2019-12-17 22:46:23 +0400

    17

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 const express = require("express");
-const bodyParser = require("body-parser");
- const app = express();
-  // создаем парсер для данных application/x-www-form-urlencoded
-const urlencodedParser = bodyParser.urlencoded({extended: false});
- app.get("/register", urlencodedParser, function (request, response) {
-    response.sendFile(__dirname + "/register.html");
+  const app = express();
+app.get("/", function(request, response){
+          response.send("<h1>Главная страница</h1>");
 });
-app.post("/register", urlencodedParser, function (request, response) {
-    if(!request.body) return response.sendStatus(400);
-    console.log(request.body);
-    response.send(`${request.body.userName} - ${request.body.userAge}`);
+app.use("/about", function(request, response){
+      
+    console.log(request.query);
+    let names = request.query.name;
+    let responseText = "<ul>";
+    for(let i=0; i < names.length; i++){
+        responseText += "<li>" + names[i] + "</li>";
+    }
+    responseText += "</ul>";
+    response.send(responseText);
 });
-  app.get("/", function(request, response){
-    response.send("Главная страница");
-});
+ app.listen(3000);
+
+
 
 
 
