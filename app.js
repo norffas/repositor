f555278b1bf2a95335d0eae2b19add0da56912db
Islam Muraev <gladskull@yandex.ru>
2019-12-17 22:37:03 +0400

    16

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
-const http = require("http");
-const fs = require("fs");
- 
-http.createServer(function(request, response){
-     
-    fs.readFile("index.html", "utf8", function(error, data){
-                 
-        let message = "Изучаем Node.js"; 
-        let header = "Главная страница";
-        data = data.replace("{header}", header).replace("{message}", message);
-        response.end(data);
-    })
-}).listen(3000);
+const express = require("express");
+const bodyParser = require("body-parser");
+ const app = express();
+  // создаем парсер для данных application/x-www-form-urlencoded
+const urlencodedParser = bodyParser.urlencoded({extended: false});
+ app.get("/register", urlencodedParser, function (request, response) {
+    response.sendFile(__dirname + "/register.html");
+});
+app.post("/register", urlencodedParser, function (request, response) {
+    if(!request.body) return response.sendStatus(400);
+    console.log(request.body);
+    response.send(`${request.body.userName} - ${request.body.userAge}`);
+});
+  app.get("/", function(request, response){
+    response.send("Главная страница");
+});
+
+
 
 
 
