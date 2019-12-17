1e5444094cf537800aa0ae7318a1c4a9c1465db2
Islam Muraev <gladskull@yandex.ru>
2019-12-17 18:43:38 +0400

    5

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const http = require("http");
 http.createServer(function(request, response){
-     response.setHeader("Content-Type", "text/html");
-    response.write("<!DOCTYPE html>");
-    response.write("<html>");
-    response.write("<head>");
-    response.write("<title>Hello Node.js</title>");
-    response.write("<meta charset=\"utf-8\" />");
-    response.write("</head>");
-    response.write("<body><h2>Привет миг</h2></body>");
-    response.write("</html>");
+        response.setHeader("Content-Type", "text/html; charset=utf-8;");
+         if(request.url === "/home" || request.url === "/"){
+        response.write("<h2>Home</h2>");    }
+    else if(request.url == "/about"){
+        response.write("<h2>About</h2>");    }
+    else if(request.url == "/contact"){
+        response.write("<h2>Contacts</h2>");    }
+    else{
+        response.write("<h2>Not found</h2>");    }
     response.end();
 }).listen(3000);
+
 
 
 
