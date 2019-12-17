9f58a4a5ee8965ac62fb6a1ae8ff955c072adfb8
Islam Muraev <gladskull@yandex.ru>
2019-12-18 00:02:31 +0400

    28

--- app.js
+++ app.js
@@ -1,17 +1,23 @@
 const express = require("express");
 const multer  = require("multer");
   const app = express();
- const upload = multer({dest:"uploads"});
-app.use(express.static(__dirname));
- app.post("/upload", upload.single("filedata"), function (req, res, next) {
+ сonst storageConfig = multer.diskStorage({
+    destination: (req, file, cb) =>{
+        cb(null, "uploads");    },
+    filename: (req, file, cb) =>{
+        cb(null, file.originalname);    }
+});
+ app.use(express.static(__dirname));
+ app.use(multer({storage:storageConfig}).single("filedata"));
+app.post("/upload", function (req, res, next) {
        let filedata = req.file;
-     console.log(filedata);
     if(!filedata)
         res.send("Ошибка при загрузке файла");
     else
         res.send("Файл загружен");
 });
-app.listen(3000);
+app.listen(3000, ()=>{console.log("Server started");});
+
 
 
 
