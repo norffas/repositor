cb750f3d26ab9c0edb74477d9c817e1b8ad83dc4
Islam Muraev <gladskull@yandex.ru>
2019-12-17 18:42:26 +0400

    4

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const http = require("http");
- http.createServer(function(request, response){
-     response.setHeader("UserId", 12);
-    response.setHeader("Content-Type", "text/html; charset=utf-8;");
-    response.write("<h2>hello world</h2>");
+http.createServer(function(request, response){
+     response.setHeader("Content-Type", "text/html");
+    response.write("<!DOCTYPE html>");
+    response.write("<html>");
+    response.write("<head>");
+    response.write("<title>Hello Node.js</title>");
+    response.write("<meta charset=\"utf-8\" />");
+    response.write("</head>");
+    response.write("<body><h2>Привет миг</h2></body>");
+    response.write("</html>");
     response.end();
 }).listen(3000);
+
 
 
 
