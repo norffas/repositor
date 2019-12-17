2c688dc23275c6601cd50ae3aad76702844e4dfa
Islam Muraev <gladskull@yandex.ru>
2019-12-17 20:32:11 +0400

    12

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 const http = require("http");
 const fs = require("fs");
  http.createServer(function(request, response){
-       console.log(`Запрошенный адрес: ${request.url}`);
-    // получаем путь после слеша
-    const filePath = request.url.substr(1);
-    fs.readFile(filePath, function(error, data){
-              
-        if(error){
-                  
-            response.statusCode = 404;
-            response.end("Resourse not found!");
-        }   
-        else{
-            response.end(data);
-        }
-    });
-}).listen(3000, function(){
-    console.log("Server started at 3000");
-});
+         console.log(`Запрошенный адрес: ${request.url}`);
+    if(request.url.startsWith("/public/")){
+                 // получаем путь после слеша
+        const filePath = request.url.substr(1);
+        fs.readFile(filePath, function(error, data){
+                if(error){
+                  response.statusCode = 404;
+                response.end("Resourse not found!");    }   
+            else{
+                response.setHeader("Content-Type", "text/html");
+                response.end(data);      }
+        })
+    }
+    else{
+        // во всех остальных случаях отправляем строку hello world!
+        response.end("Hello World!");
+    }
+
 
 
 
