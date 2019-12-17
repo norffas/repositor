ef853f36f8c23f44851a3b7a944436cef1554639
Islam Muraev <gladskull@yandex.ru>
2019-12-17 18:46:45 +0400

    6

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 const http = require("http");
-http.createServer(function(request, response){
-        response.setHeader("Content-Type", "text/html; charset=utf-8;");
-         if(request.url === "/home" || request.url === "/"){
-        response.write("<h2>Home</h2>");    }
-    else if(request.url == "/about"){
-        response.write("<h2>About</h2>");    }
-    else if(request.url == "/contact"){
-        response.write("<h2>Contacts</h2>");    }
+  http.createServer(function(request, response){
+       response.setHeader("Content-Type", "text/html; charset=utf-8;");
+       if(request.url === "/"){
+        response.statusCode = 302; // временная переадресация
+        // на адрес localhost:3000/newpage
+        response.setHeader("Location", "/newpage");
+    }
+    else if(request.url == "/newpage"){
+        response.write("New address");
+    }
     else{
-        response.write("<h2>Not found</h2>");    }
+        response.write("Not Found");
+        response.statusCode = 404; // адрес не найден
+    }
     response.end();
 }).listen(3000);
+
 
 
 
