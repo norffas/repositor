5147c10fb4509abafa7267508128eecf55864e04
Islam Muraev <gladskull@yandex.ru>
2019-12-17 23:30:17 +0400

    22

--- app.js
+++ app.js
@@ -1,19 +1,14 @@
 const express = require("express");
 const app = express();
- app.use("/about", function (request, response) {
-  response.send("О сайте");
+// создаем парсер для данных в формате json
+const jsonParser = express.json();
+app.post("/user", jsonParser, function (request, response) {
+console.log(request.body);
+if(!request.body) return response.sendStatus(400);
+response.json(request.body); // отправляем пришедший ответ обратно
 });
- app.use("/products/create",function (request, response) {
-  response.send("Добавление товара");
-});
-app.use("/products/:id",function (request, response) {
-  response.send(`Товар ${request.params.id}`);
-});
-app.use("/products/",function (request, response) {
-  response.send("Список товаров");
-});
- app.use("/", function (request, response) {
-  response.send("Главная страница");
+app.get("/", function(request, response){
+response.sendFile(__dirname + "/index.html");
 });
 app.listen(3000);
 
