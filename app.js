6120122eb8743c1114bcb86e09261e1e7c0e3729
Islam Muraev <gladskull@yandex.ru>
2019-12-17 18:41:10 +0400

    3

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-var http = require("http");
-http.createServer(function(request, response){
-    console.log("Url: " + request.url);
-    console.log("Тип запроса: " + request.method);
-    console.log("User-Agent: " + request.headers["user-agent"]);
-    console.log("Все заголовки");
-    console.log(request.headers);
+const http = require("http");
+ http.createServer(function(request, response){
+     response.setHeader("UserId", 12);
+    response.setHeader("Content-Type", "text/html; charset=utf-8;");
+    response.write("<h2>hello world</h2>");
     response.end();
 }).listen(3000);
+
 
 
