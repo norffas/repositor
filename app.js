38f260be86735b9024364f39424372d80d2dc74f
Islam Muraev <gladskull@yandex.ru>
2019-12-17 23:40:43 +0400

    24

--- app.js
+++ app.js
@@ -1,22 +1,14 @@
 const express = require("express");
 const app = express();
-const userController = require("./controllers/userController.js");
-const homeController = require("./controllers/homeController.js");
- // определяем Router
-const userRouter = express.Router();
-const homeRouter = express.Router();
-  // определяем маршруты и их обработчики внутри роутера userRouter
-userRouter.use("/create", userController.addUser);
-userRouter.use("/", userController.getUsers);
-app.use("/users", userRouter);
- // определяем маршруты и их обработчики внутри роутера homeRouter
-homeRouter.get("/about", homeController.about);
-homeRouter.get("/", homeController.index);
+const userRouter = require("./routes/userRouter.js");
+const homeRouter = require("./routes/homeRouter.js");
+ app.use("/users", userRouter);;
 app.use("/", homeRouter);
  app.use(function (req, res, next) {
     res.status(404).send("Not Found")
 });
  app.listen(3000);
+
 
 
 
